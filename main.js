50cc2dc3f349361bc22dce8e41d22f289ba0e29f
sergio mosquera <sergioal18@hotmail.com>
2023-03-17 21:28:58 -0500

    se fucionan los menus

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-Menu');
+const desktopMenu = document.querySelector('.desktop-menu');
 
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+}
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
-function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive')
 
-}
+    
 
