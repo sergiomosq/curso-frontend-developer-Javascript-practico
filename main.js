a944759825321872f2b6025b874f9ce2c2871727
sergio mosquera <sergioal18@hotmail.com>
2023-03-17 22:32:52 -0500

    funcionamiento del menu hamburguesa para mobile

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', togglemobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
+function togglemobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
+
 
 
     
